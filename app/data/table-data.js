cf77dfc00d4d1d4da9c225fc18788f33df448200
Brendan Kellams <brendan.kellams@gmail.com>
2018-02-27 12:17:26 -0800

    changing things up for hot restaurant

---
+++ app/data/table-data.js
@@ -0,0 +1,10 @@
+var tableArray = [
+  {
+    customerName: "Major Butthead",
+    customerEmail: "major@butthead.com",
+    customerID: "meow",
+    phoneNumber: '000-000-0000'
+  }
+];
+
+module.exports = tableArray;
